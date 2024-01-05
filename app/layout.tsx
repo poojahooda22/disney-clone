93a0efcb574f17e090d71efa6009bb2aac3de75c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-05 15:19:53 +0530

    updated app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header';
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <ThemeProvider
-            attribute="class"
+        attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
