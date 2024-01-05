53bbe07be3368b68d50345699f7008fe7561cb5f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-05 13:29:36 +0530

    updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,15 @@ import Image from 'next/image';
 function Header() {
   return (
     <header>
-      
+      <Link href="/">
+        <Image
+          src="https://links.papereact.com/a943ae"
+          alt="Disney+"
+          
+          width={80}
+            height={80}
+        />
+      </Link>
     </header>
     
   )
