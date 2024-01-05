610320ca9ad8924a83afb529b88424ec3cbeeb1a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-05 15:27:53 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+          enableSystem
+          disableTransitionOnChange
         >
           <Header />
           {children}
