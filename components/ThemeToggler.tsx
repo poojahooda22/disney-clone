b085956d23a69410078ca43e282874c895a9ff70
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-06 20:52:46 +0530

    updated

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -19,6 +19,7 @@ export function ThemeToggler() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           
         </Button>
       </DropdownMenuTrigger>
