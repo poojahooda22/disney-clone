be71b6d28ebe6b7e97b836103f237ce50acea20b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-06 21:06:45 +0530

    updated dropdown

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -6,7 +6,7 @@ import { Check, ChevronRight, Circle } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-// const DropdownMenu = DropdownMenuPrimitive.Root
+const DropdownMenu = DropdownMenuPrimitive.Root
 
 // const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
 
