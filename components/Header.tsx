6c349d6b1680bc08043c614e411683addde3818d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-07 11:23:28 +0530

    updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import {ThemeToggler} from './ThemeToggler';
+import SearchInput from './SearchInput';
 
 function Header() {
   return (
-    <header className='fixed w-full z-20 top-0 flex items-center justify-center p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900'>
+    <header className='fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900'>
       <Link href="/">
         <Image
           src="https://links.papareact.com/a943ae"
@@ -16,12 +17,12 @@ function Header() {
         />
       </Link>
       <div 
-      className='flex space-x-2'>
+        className='flex space-x-2 items-center gap-4'
+      >
         {/* GenreDropdown*/}
-        {/* SearchBar*/}
+        <SearchInput/>
         <ThemeToggler />
         {/* Login*/}
-
       </div>
     </header>    
   )
