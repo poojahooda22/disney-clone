dd724fb878f045fb9d57bd8a1b673ba7b3550695
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-07 13:03:10 +0530

    updated searchinput

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,14 +1,25 @@
 "use Client";
 
 import React from 'react';
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from 'zod';
 import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
 
 const formSchema = z.object({
   input: z.string().min(2).max(50)
 })
 
 function SearchInput() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      input: "",
+    },
+  });
+
+  
   const router = useRouter();
   return (
     <div>
