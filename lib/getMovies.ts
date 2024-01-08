6452dd6b6f1ec71014d91032856d2fa8ade3310c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-09 00:29:10 +0530

    updated movies

--- lib/getMovies.ts
+++ lib/getMovies.ts
@@ -4,4 +4,15 @@ async function fetchFromTMDB(url: URL, cacheTime?: number) {
     url.searchParams.set("sort_by", "popularity.desc");
     url.searchParams.set("language", "en-US");
     url.searchParams.set("page", "1");
+
+    const options: RequestInit = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+        },
+        next: {
+            revalidate: cacheTime || 60 * 60 * 24, //24 hours
+        }
+    }
 }
