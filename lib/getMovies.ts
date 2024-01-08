3616483b450e5b4f86e689e8ea961a6e46d3bf55
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-09 00:26:52 +0530

    created get movies

---
+++ lib/getMovies.ts
@@ -0,0 +1,7 @@
+async function fetchFromTMDB(url: URL, cacheTime?: number) {
+    url.searchParams.set("include_adult", "false");
+    url.searchParams.set("include_video", "false");
+    url.searchParams.set("sort_by", "popularity.desc");
+    url.searchParams.set("language", "en-US");
+    url.searchParams.set("page", "1");
+}
