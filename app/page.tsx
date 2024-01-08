5aea1a6961ff66bba9b57a796b86af31743a4bc0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-09 00:41:23 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { getUpcomingMovies } from '@/lib/getMovies';
 
 export default async function Home() {
   const upComingMovies = await getUpcomingMovies();
-  // const topRatedMovies = await getTopRatedMovies();
+  const topRatedMovies = await getTopRatedMovies();
   // const popularMovies = await getPopularMovies();
   return (
     <main >
@@ -14,6 +14,7 @@ export default async function Home() {
 
       <div className='flex flex-col space-y-2 xl:-mt-48'>
         <MoviesCarousel movies={upComingMovies} title="Upcoming" />
+        <MoviesCarousel movies={} title="Top Rated" />
         {/* <MoviesCarousel for top rated movie /> */}
         {/* <MoviesCarousel for popular movie /> */}
       </div>
