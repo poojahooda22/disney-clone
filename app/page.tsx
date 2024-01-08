5d9451a665aa7809f77b3b632ae1544974a6eaa1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-09 00:26:42 +0530

    updated app

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
 import MoviesCarousel from '@/components/MoviesCarousel';
 
-export default function Home() {
+export default async function Home() {
+  const upComingMovies = await getUpcomiongMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
   return (
     <main >
       
 
       {/* CarouselBannerWrapper */}
 
-      <div>
+      <div className='flex flex-col space-y-2 xl:-mt-48'>
         <MoviesCarousel movies={[]} title="Upcoming" />
         {/* <MoviesCarousel for top rated movie /> */}
         {/* <MoviesCarousel for popular movie /> */}
