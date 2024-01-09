16caf897f3f8d4fd0f46574ecd231603cdbc68f5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-10 00:46:30 +0530

    updated

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -17,7 +17,7 @@ function MoviesCarousel({title, movies, isVertical}: Props) {
         <div 
           className={cn(
             "flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide",
-            isVertical && "flex-col"
+            isVertical && "flex-col space-x-0"
           )}
         >
           {movies?.map(movie => (
