30f3295fe0319b9f48340ed7edff818e303efc6f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-10 00:44:25 +0530

    updated carousel

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -14,7 +14,8 @@ function MoviesCarousel({title, movies, isVertical}: Props) {
     <div className='z-50'>
         <h2 className='text-xl font-bold  px-10 py-2'>{title}</h2>
 
-        <div className={cn("flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide")}>
+        <div 
+        className={cn("flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide")}>
           {movies?.map(movie => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
