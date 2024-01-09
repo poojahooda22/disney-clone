7ee65c7f4715918e6c62daacb0df37fbeb3ff84a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-10 00:08:39 +0530

    updated movie carousel

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -1,6 +1,7 @@
 import { Movie } from '@/typings';
 import React from 'react';
 import MovieCard from './MovieCard';
+import { cn } from "@/lib/utils";
 
 type Props = {
     title?: string;
@@ -11,10 +12,13 @@ type Props = {
 function MoviesCarousel({title, movies, isVertical}: Props) {
   return (
     <div className='z-50'>
-        <h2>{title}</h2>
-        {movies?.map(movie => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        <h2 className='text-xl font-bold px-10 py-2'>{title}</h2>
+
+        <div className={cn("flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide")}>
+          {movies?.map(movie => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>  
     </div>
   )
 }
