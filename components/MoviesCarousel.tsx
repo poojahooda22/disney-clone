82d61e2ca19e4cf47f5615b9ac78166a98e77923
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-10 00:40:39 +0530

    updated

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -12,7 +12,7 @@ type Props = {
 function MoviesCarousel({title, movies, isVertical}: Props) {
   return (
     <div className='z-50'>
-        <h2 className='text-xl font-bold px-10 py-2'>{title}</h2>
+        <h2 className='text-xl '>{title}</h2>
 
         <div className={cn("flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide")}>
           {movies?.map(movie => (
