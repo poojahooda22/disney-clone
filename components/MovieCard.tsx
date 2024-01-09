954ad2779c808b1d6bd771265474ad1377fdb453
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-10 00:08:27 +0530

    updated movie card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
 import { Movie } from '@/typings'
-import Image from 'next/image'
+import Image from 'next/image';
+import  getImagePath  from '@/lib/getImagePath';
 
 function MovieCard({movie}: {movie: Movie}) {
   return (
-    <div>
-      <p>{movie.title}</p>
+    <div className='relative flex-shrink-0 cursor-pointer transform hover:scale-105 transition duration-200 ease-out hover:drop-shadow-lg'>
+        <div className="absolute inset-0 bg-gradient-to-b from-gray-200/0
+         via-gray-900/10 to-gray-300 dark:to-[#1A1C29]/80 z-10" />
+      <p className='absolute z-20 bottom-5 left-5'>{movie.title}</p>
 
       <Image
+        className='w-fit lg:min-w-[400px] h-56 object-cover object-center shadow-md 
+        shadow-gray-900 drop-shadow-xl rounded-lg'
         src={getImagePath(movie.backdrop_path || movie.poster_path)}
         alt={movie.title}
         width={1920}
