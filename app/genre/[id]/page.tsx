df983f4fc34efc9133f7fcb49e900f0e1f3c33e1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-11 00:11:24 +0530

    updated app

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { getDiscoverMovies } from '@/lib/getMovies';
+import MoviesCarousel from '@/components/MoviesCarousel';
 
 type Props = {
     params: {
@@ -9,10 +11,15 @@ type Props = {
     }
 }
 
-function GenrePage({ params: { id }, searchParams: {genre}}: Props) {
+async function GenrePage({ params: { id }, searchParams: {genre}}: Props) {
+  const movies = await getDiscoverMovies(id);
   return (
-    <div className='text-red-500'>
-      Welcome to the genre with id: {id} and name: {genre}
+    <div className='max-w-7xl mx-auto' >
+     
+     <div className='flex flex-col space-y-4 mt-32 xl:mt-42'>
+      <h1 className='text-6xl font-semibold px-10'>Results for {genre}</h1>
+      <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+     </div>
     </div>
   )
 }
