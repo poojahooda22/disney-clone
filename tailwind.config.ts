b62d4cbabf8d35cb4a2ce77b52544eb10e695e49
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-11 00:13:05 +0530

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      flex: {
+        full: "0 0 100%",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
