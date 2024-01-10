b76c5dfa61d1f212669f906e6b72214106e9e58b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-11 00:12:23 +0530

    updated carousel

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Movie } from '@/typings';
-import React from 'react';
+
+import React, { useRef, useEffect } from 'react';
 import MovieCard from './MovieCard';
 import { cn } from "@/lib/utils";
 
@@ -10,11 +13,55 @@ type Props = {
 }
 
 function MoviesCarousel({title, movies, isVertical}: Props) {
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const slider = sliderRef.current;
+    if (!slider) return;
+
+    let isDown = false;
+    let startX: number;
+    let scrollLeft: number;
+
+    const startDragging = (e: MouseEvent) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    };
+
+    const stopDragging = () => {
+      isDown = false;
+      slider.classList.remove('active');
+    };
+
+    const whileDragging = (e: MouseEvent) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3;
+      slider.scrollLeft = scrollLeft - walk;
+    };
+
+    slider.addEventListener('mousedown', startDragging);
+    slider.addEventListener('mouseleave', stopDragging);
+    slider.addEventListener('mouseup', stopDragging);
+    slider.addEventListener('mousemove', whileDragging);
+
+    return () => {
+      slider.removeEventListener('mousedown', startDragging);
+      slider.removeEventListener('mouseleave', stopDragging);
+      slider.removeEventListener('mouseup', stopDragging);
+      slider.removeEventListener('mousemove', whileDragging);
+    };
+  }, []);
+
   return (
     <div className='z-50'>
         <h2 className='text-xl font-bold  px-10 py-2'>{title}</h2>
 
         <div 
+          ref={sliderRef}
           className={cn(
             "flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide",
             isVertical && "flex-col space-x-0 space-y-12"
