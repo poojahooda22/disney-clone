309bb2631d9803cc4cbcba931b509f0b3e9fdab4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-11 00:12:12 +0530

    created carousel banner

---
+++ components/CarouselBannerWrapper.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getDiscoverMovies } from '@/lib/getMovies';
+import CarouselBanner from './CarouselBanner';
+
+type Props = {
+  id?: string;
+  keywords?: string;
+}
+
+async function CarouselBannerWrapper({id, keywords}: Props) {
+  const movies = await getDiscoverMovies(id, keywords);
+  return <CarouselBanner movies={movies} />
+}
+
+export default CarouselBannerWrapper;
