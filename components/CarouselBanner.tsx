385ba58c985e233aee04b69a90b2cb7b168a72b4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-25 21:38:15 +0530

    updated banner

--- components/CarouselBanner.tsx
+++ components/CarouselBanner.tsx
@@ -43,7 +43,8 @@ function CarouselBanner({movies}: {movies: Movie[]}) {
                         p-10 space-y-5 text-white'
                     >
                         <h2 
-                            className='text-3xl font-bold max-w-xl z-50'
+                            className='text-3xl font-bold 
+                            max-w-xl z-50'
                             >
                             {movie.title}
                         </h2>
