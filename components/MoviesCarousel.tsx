c7f8ef000dbf72ab4320eed1a4641ec91469d597
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-25 22:12:46 +0530

    add

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -58,7 +58,8 @@ function MoviesCarousel({title, movies, isVertical}: Props) {
 
   return (
     <div className='z-50'>
-        <h2 className='text-xl font-bold  
+        <h2 
+        className='text-xl font-bold  
           px-10 py-2'>
           {title}
           </h2>
